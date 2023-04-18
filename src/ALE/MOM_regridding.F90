24dbae3ac5a7d80fc94d8634f2c4117b8eda3b3e
Graeme MacGilchrist <graemem@princeton.edu>
2023-04-18 15:23:13 -0400

    Indentation fix

--- src/ALE/MOM_regridding.F90
+++ src/ALE/MOM_regridding.F90
@@ -2025,7 +2025,7 @@ end subroutine convective_adjustment
 !> Return the index of a sorted array of scalar values
 subroutine sort_scalar_k_1d(nz, phi, ksort)
   integer,                 intent(in)     :: nz !< Number of levels n grid
-    real, dimension(nz),   intent(inout)  :: phi  !< Array of scalar quantity to be sorted
+  real, dimension(nz),     intent(inout)  :: phi  !< Array of scalar quantity to be sorted
   integer, dimension(nz),  intent(out)    :: ksort !< An array of indicies for a 
                                                   !! monotonically increasing scalar
 !------------------------------------------------------------------------------
